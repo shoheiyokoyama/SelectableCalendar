22c91f1a8a7f3a9221fe7c650329d5ae345b8798
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-09-04 13:48:54 +0900

    initial commit

---
+++ SlectableCalender/SlectableCalender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlectableCalender
+//
+//  Created by 横山祥平 on 2016/09/04.
+//  Copyright © 2016年 Shohei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
